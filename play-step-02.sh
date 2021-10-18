e94f915f8782283bfacce243a4500031153b34e1
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-18 18:49:47 +0200

    Update play-step-02.sh

--- play-step-02.sh
+++ play-step-02.sh
@@ -1,3 +1,3 @@
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_hypervisor/main.yml
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_vm/main.yml
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_switchs/main.yml
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_switches/main.yml
