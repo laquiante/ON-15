924da0b6128cefd8e9422da9daeb23ed4d64eccd
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-18 19:10:21 +0200

    Update vswitches.sh

--- step-02/prepare_hypervisor/vswitches.sh
+++ step-02/prepare_hypervisor/vswitches.sh
@@ -1,15 +1,22 @@
 # etwas spass muss sein
 # 24-SEP-2021 : 20:27
 #
+# create the bridges
 ip link add vSwitch1 type bridge vlan_filtering 1
 ip link add vSwitch2 type bridge vlan_filtering 1
+ip link set dev vSwitch1 up
+ip link set dev vSwitch2 up
+# downlinks
 ip link set dev eth3 master vSwitch1
 ip link set dev eth4 master vSwitch2
 ip link set dev eth5 master vSwitch1
 ip link set dev eth6 master vSwitch2
-ip link set dev vSwitch1 up
-ip link set dev vSwitch2 up
 ip link set dev eth3 up
 ip link set dev eth4 up
 ip link set dev eth5 up
 ip link set dev eth6 up
+# uplinks
+ip link set dev eth1 master vSwitch1
+ip link set dev eth2 master vSwitch2
+ip link set dev eth1 up
+ip link set dev eth2 up
