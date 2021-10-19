13d90d56a3b80973903f38e79a8ff8bd87a25c90
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-19 17:20:41 +0200

    Update play-step-03.sh

--- play-step-03.sh
+++ play-step-03.sh
@@ -6,3 +6,9 @@ ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf03
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf04
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf05
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf06
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/storage01
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/server11
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/server12
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/server13
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/vm01
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/vm02
