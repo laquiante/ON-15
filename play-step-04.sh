a36365cc96ffc7893669808a5399dda67709c7b8
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-21 13:45:19 +0200

    Update play-step-04.sh

--- play-step-04.sh
+++ play-step-04.sh
@@ -1,3 +1,4 @@
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/leaf01
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/leaf02
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/spine01
+#ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/leaf01
+#ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/leaf02
+#ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/spine01
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/step-04-classic
