3541d267ff58970f2665929a20125e2a007ab476
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-29 17:50:33 +0200

    Update play-step-06c-student-lab.sh

--- play-step-06c-student-lab.sh
+++ play-step-06c-student-lab.sh
@@ -1,2 +1,5 @@
-# calling playbook to run all plus leaf01 with jinja template as a reference
+#
+# calling playbook to run all leaf's minus leaf01 which shows a jinja template for the loopback as a reference
+#
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06c/step-06c-student-linux-classic.yaml
+#
