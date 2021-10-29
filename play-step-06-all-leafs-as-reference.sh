2c67caac6b96710e92a4a3ee40f0bf2c6a21220d
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-29 19:10:48 +0200

    Update and rename play-step-06-all-leafs-as-reference.sh to play-step-06a-all-leafs-as-reference.sh

--- play-step-06-all-leafs-as-reference.sh
+++
@@ -1,5 +0,0 @@
-#
-# reference
-#
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06/step-06-classic
-#
---
+++ play-step-06a-all-leafs-as-reference.sh
@@ -0,0 +1,5 @@
+#
+# reference
+#
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06/step-06a-classic
+#
