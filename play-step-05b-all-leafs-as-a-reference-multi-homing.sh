7aba4d94d09ec5b88afd5c8685e312425399caa4
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-29 20:11:40 +0200

    Update play-step-05b-all-leafs-as-a-reference-multi-homing.sh

--- play-step-05b-all-leafs-as-a-reference-multi-homing.sh
+++ play-step-05b-all-leafs-as-a-reference-multi-homing.sh
@@ -1,1 +1,5 @@
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05b-mh-classic
+#
+# step-05step-05b-mh-student-linux-classic
+#
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05b-mh-student-linux-classic
+#
