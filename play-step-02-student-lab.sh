2b3f7519b1d40a55f916be872642996f1f46a02d
laquiante <33266194+laquiante@users.noreply.github.com>
2021-10-29 22:02:08 +0200

    Create play-step-02-student-lab.sh

---
+++ play-step-02-student-lab.sh
@@ -0,0 +1,3 @@
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_hypervisor/main.yml
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_vm/main.yml
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_switches/student.yml
