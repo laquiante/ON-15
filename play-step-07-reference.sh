477f19508fb7d6d1390180c36241c897a597676c
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-01 14:53:40 +0100

    Update play-step-07-reference.sh

--- play-step-07-reference.sh
+++ play-step-07-reference.sh
@@ -1,2 +1,2 @@
-# ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-07/playbooks/main.yaml
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-07/main.yaml
+ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-07/playbooks/main.yaml
+# ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-07/main.yaml
