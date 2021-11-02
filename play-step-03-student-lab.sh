a3222d7044fe9964feeaa74ea407f673c8d7b600
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-02 14:31:28 +0100

    Update play-step-03-student-lab.sh

--- play-step-03-student-lab.sh
+++ play-step-03-student-lab.sh
@@ -1,7 +1,11 @@
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/spine01
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/spine02
-# the following playbook "leaf01-student" needs work
+
+################################################################################
+# the following playbook "leaf01-student" and maybe dependent files needs work #
+################################################################################
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf01-student
+
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf02
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf03
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-03/leaf04
