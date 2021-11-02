3f9c64ed5771c30257e04d64218ed97a7fced7a8
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-02 14:34:47 +0100

    Update play-step-05b-student-lab-multi-homing.sh

--- play-step-05b-student-lab-multi-homing.sh
+++ play-step-05b-student-lab-multi-homing.sh
@@ -1,1 +1,4 @@
+#############################################################################################
+# the following playbook "step-05b-mh-student-classic" and maybe dependent files needs work #
+#############################################################################################
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05b-mh-student-classic
