8df8ff246307abf657bf76fa4de50b8eeafcadbe
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-02 14:32:51 +0100

    Update play-step-04-student-lab.sh

--- play-step-04-student-lab.sh
+++ play-step-04-student-lab.sh
@@ -1,1 +1,4 @@
+################################################################################
+# the following playbook "step-04-student" and maybe dependent files needs work #
+################################################################################
 ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/step-04-student
