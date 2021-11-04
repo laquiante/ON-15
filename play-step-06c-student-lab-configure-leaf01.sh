63081ef698c6d5235053407e4d4a88d6761c4e53
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-04 07:08:56 +0100

    Update play-step-06c-student-lab-configure-leaf01.sh

--- play-step-06c-student-lab-configure-leaf01.sh
+++ play-step-06c-student-lab-configure-leaf01.sh
@@ -1,5 +1,5 @@
 ################################################################################################
 # the following playbook "step-06c-student-linux-classic" and maybe dependent files needs work #
 ################################################################################################
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06c/step-06c-student-linux-classic.yaml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06c/step-06c-student-linux-classic.yaml
 #
