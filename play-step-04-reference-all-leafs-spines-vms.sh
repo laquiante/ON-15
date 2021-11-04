35658d2eefec5c78c54c6c103fba043bcea13cde
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-04 07:04:25 +0100

    Update play-step-04-reference-all-leafs-spines-vms.sh

--- play-step-04-reference-all-leafs-spines-vms.sh
+++ play-step-04-reference-all-leafs-spines-vms.sh
@@ -1,4 +1,1 @@
-#ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/leaf01
-#ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/leaf02
-#ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/spine01
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/step-04-classic
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/step-04-classic
