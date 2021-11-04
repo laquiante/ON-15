44493a607c0240c648bfe7775ef935af284cd761
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-04 07:05:54 +0100

    Update play-step-05a-reference-l2-all-leafs.sh

--- play-step-05a-reference-l2-all-leafs.sh
+++ play-step-05a-reference-l2-all-leafs.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05a-l2-classic
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05a-l2-classic
