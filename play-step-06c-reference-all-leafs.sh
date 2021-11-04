b1053cb00d4aaa7cee06a0b84c1293720d7e8f57
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-04 07:08:39 +0100

    Update play-step-06c-reference-all-leafs.sh

--- play-step-06c-reference-all-leafs.sh
+++ play-step-06c-reference-all-leafs.sh
@@ -1,5 +1,5 @@
 #
 # calling playbook to run all leaf's minus leaf01 which shows a jinja template for the loopback as a reference
 #
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06c/step-06c-all-leafs-as-reference.yaml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06c/step-06c-all-leafs-as-reference.yaml
 #
