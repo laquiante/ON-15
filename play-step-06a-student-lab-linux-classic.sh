642a9ea1e971186545ccdad2d2a0de0886201a13
laquiante <33266194+laquiante@users.noreply.github.com>
2021-11-04 07:08:07 +0100

    Update play-step-06a-student-lab-linux-classic.sh

--- play-step-06a-student-lab-linux-classic.sh
+++ play-step-06a-student-lab-linux-classic.sh
@@ -1,5 +1,5 @@
 ###############################################################################################
 # the following playbook "step-06a-student-classic.yaml" and maybe dependent files needs work #
 ###############################################################################################
-ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06/step-06a-student-linux-classic.yaml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06/step-06a-student-linux-classic.yaml
 #
