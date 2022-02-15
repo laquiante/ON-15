edb186d990495b0b2cfcad8ae1658bafdcb3187c
tomerlior <71632679+tomerlior@users.noreply.github.com>
2022-02-15 18:02:43 +0200

    Updated playbook execution to escalate privileges

--- play-step-07-reference-roles-templates-vars.sh
+++ play-step-07-reference-roles-templates-vars.sh
@@ -1,2 +1,2 @@
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-07/playbooks/main.yaml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-07/playbooks/main.yaml
 
