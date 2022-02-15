0df2f4aa8f397c117645f198ecb694c95d2c7524
tomerlior <71632679+tomerlior@users.noreply.github.com>
2022-02-15 17:59:17 +0200

    Updated playbook execution to escalate privileges

--- play-step-04-reference-all-leafs-spines-vms.sh
+++ play-step-04-reference-all-leafs-spines-vms.sh
@@ -1,1 +1,1 @@
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-04/step-04-classic
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-04/step-04-classic
