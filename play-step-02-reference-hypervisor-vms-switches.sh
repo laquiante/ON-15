c547fef1dd823ddf26c9588af74a0214444786f8
tomerlior <71632679+tomerlior@users.noreply.github.com>
2022-02-15 17:55:48 +0200

    Updated playbooks execution to escalate privileges

--- play-step-02-reference-hypervisor-vms-switches.sh
+++ play-step-02-reference-hypervisor-vms-switches.sh
@@ -1,3 +1,3 @@
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_hypervisor/main.yml
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_vm/main.yml
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_switches/main.yml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-02/prepare_hypervisor/main.yml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-02/prepare_vm/main.yml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-02/prepare_switches/main.yml
