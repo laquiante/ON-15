939a835464559e6f509bcdcc23f030dab5a1712c
tomerlior <71632679+tomerlior@users.noreply.github.com>
2022-02-15 18:01:57 +0200

    Updated playbook execution to escalate privileges

--- play-step-06b-reference-all-leafs.sh
+++ play-step-06b-reference-all-leafs.sh
@@ -1,4 +1,4 @@
 #
 # this play comes without a student task to show the speed
 #
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-06b/step-06-classic
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-06b/step-06-classic
