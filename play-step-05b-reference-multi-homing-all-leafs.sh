5e4a5a461a22f1ef002c5885e80749a26b76935a
tomerlior <71632679+tomerlior@users.noreply.github.com>
2022-02-15 18:00:09 +0200

    Updated playbooks execution to escalate privileges

--- play-step-05b-reference-multi-homing-all-leafs.sh
+++ play-step-05b-reference-multi-homing-all-leafs.sh
@@ -1,5 +1,5 @@
 #
 # step-05step-05b-mh
 #
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05b-mh-linux-classic
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-05/step-05b-mh-linux-classic
 #
