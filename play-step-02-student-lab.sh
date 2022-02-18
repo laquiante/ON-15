6971b6ed173472d7277008bd0d87253790b14bd6
laquiante <33266194+laquiante@users.noreply.github.com>
2022-02-18 08:28:10 +0100

    Update play-step-02-student-lab.sh

--- play-step-02-student-lab.sh
+++ play-step-02-student-lab.sh
@@ -1,6 +1,6 @@
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_hypervisor/main.yml
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_vm/main.yml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-02/prepare_hypervisor/main.yml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-02/prepare_vm/main.yml
 #############################################################################
 # the following playbook "student.yml" and maybe dependent files needs work #
 #############################################################################
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-02/prepare_switches/student.yml
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-02/prepare_switches/student.yml
