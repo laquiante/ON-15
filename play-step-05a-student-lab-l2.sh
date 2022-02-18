f848960b1a59320a29c16b14b629c1d3d3554714
laquiante <33266194+laquiante@users.noreply.github.com>
2022-02-18 08:30:24 +0100

    Update play-step-05a-student-lab-l2.sh

--- play-step-05a-student-lab-l2.sh
+++ play-step-05a-student-lab-l2.sh
@@ -1,4 +1,4 @@
 #############################################################################################
 # the following playbook "step-05a-student-l2-classic" and maybe dependent files needs work #
 #############################################################################################
-sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts ./step-05/step-05a-student-l2-classic
+sudo ansible-playbook -i /home/cumulus/ON-15/inventory/files/hosts -b ./step-05/step-05a-student-l2-classic
